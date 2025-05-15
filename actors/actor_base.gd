9c0725ec1cd340390dbccbc4935e18b47fd22880
Freddy Ebermann <freddy@42berlin.de>
2025-05-15 16:34:09 +0200

    added stats to actor_base

--- actors/actor_base.gd
+++ actors/actor_base.gd
@@ -1,3 +1,5 @@
 class_name ActorBase extends Resource
 
-@export var sprite:Texture2D
+@export var sprite: Texture2D
+
+@export var stats: Stats
---
+++ stats.gd
@@ -0,0 +1,8 @@
+class_name Stats extends Resource
+
+@export var health: int
+@export var energy: int
+@export var attack: int
+@export var intelligence: int
+@export var defence: int
+@export var resistance: int
