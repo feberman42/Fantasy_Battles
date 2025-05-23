b23c4f45c5dbe6a39532ffc8214f6898aac52500
Frederik Ebermann <110936735+feberman42@users.noreply.github.com>
2025-05-23 14:10:37 +0200

    restructure actor resources

--- refs.gd
+++ refs.gd
@@ -7,9 +7,12 @@ func _ready() -> void:
 	_fetch_actor_base_list()
 
 func _fetch_actor_base_list() -> void:
-	var file_names: PackedStringArray = ResourceLoader.list_directory(ACTOR_BASE_PATH)
-	for file in file_names:
-		if not file.ends_with(".tres"): continue
-		var actor_base: ActorBase = ResourceLoader.load(ACTOR_BASE_PATH + "/" + file)
+	var actor_dirs: PackedStringArray = ResourceLoader.list_directory(ACTOR_BASE_PATH)
+	for actor_dir in actor_dirs:
+		var file: String = actor_dir.rstrip("/") + ".tres"
+		var actor_base: ActorBase = ResourceLoader.load(ACTOR_BASE_PATH + "/" + actor_dir + file)
+		if not actor_base:
+			print("Couldn't load: ", file)
+			continue
 		ACTOR_BASE_LIST.append(actor_base)
 		print(actor_base.resource_path, " loaded.")
