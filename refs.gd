721fb6c518b38fb0c60dda7a8ad17cb799eccc99
Freddy Ebermann <freddy@42berlin.de>
2025-05-26 12:26:51 +0200

    fixed resource check

--- refs.gd
+++ refs.gd
@@ -10,6 +10,9 @@ func _fetch_actor_base_list() -> void:
 	var actor_dirs: PackedStringArray = ResourceLoader.list_directory(ACTOR_BASE_PATH)
 	for actor_dir in actor_dirs:
 		var file: String = actor_dir.rstrip("/") + ".tres"
+		var path: String = ACTOR_BASE_PATH + "/" + actor_dir + file
+		if not ResourceLoader.exists(path):
+			continue
 		var actor_base: ActorBase = ResourceLoader.load(ACTOR_BASE_PATH + "/" + actor_dir + file)
 		if not actor_base:
 			print("Couldn't load: ", file)
