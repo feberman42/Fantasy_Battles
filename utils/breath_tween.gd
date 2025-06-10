56bba51235d6f7cfa5f4d53343d9b05015140ac3
Freddy Ebermann <freddy@42berlin.de>
2025-06-10 11:08:13 +0200

    added breather tween for animation

---
+++ utils/breath_tween.gd
@@ -0,0 +1,17 @@
+class_name BreathTween extends Node
+
+@export var intensity: Vector2 = Vector2(1.02, 0.98)
+@export var speed: float = 1.0
+
+var tween: Tween
+@onready var parent = get_parent()
+
+func _ready():
+	tween = create_tween()
+	tween.set_loops()  # Loop forever
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_property(parent, "scale", intensity, speed)
+	tween.tween_property(parent, "scale", Vector2(1.0, 1.0), speed)
+	
