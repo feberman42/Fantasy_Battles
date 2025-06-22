fb2daa1b8421e91b4d105b73993a3c8d9ffdab96
Frederik Ebermann <110936735+feberman42@users.noreply.github.com>
2025-06-22 03:47:37 +0200

    rename menu signal

--- signals.gd
+++ signals.gd
@@ -1,10 +1,10 @@
 extends Node
 
 signal money_changed(amount: int)
-signal show_menu(menu: Refs.Menu)
+signal toggle_menu(menu: Refs.Menu)
 
 func _ready() -> void:
-	show_menu.connect(log_show_menu)
+	toggle_menu.connect(log_show_menu)
 	
 func log_show_menu(id: Refs.Menu) -> void:
 	print("Menu show: ", id)
