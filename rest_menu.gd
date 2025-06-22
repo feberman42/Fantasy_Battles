ea1eb582eda241b1e2066d9c6b593a7204e08702
Frederik Ebermann <110936735+feberman42@users.noreply.github.com>
2025-06-22 16:40:24 +0200

    added lvl up notify to rest menu

--- rest_menu.gd
+++ rest_menu.gd
@@ -1,6 +1,7 @@
 extends BaseMenu
 
 @onready var battle_manager: BattleScene = %BattleScene
+@onready var level_up_notification: Control = %LevelUpNotification
 
 func _ready() -> void:
 	super._ready()
@@ -12,7 +13,12 @@ func _process(delta: float) -> void:
 
 func _update() -> void:
 	%Shop.update()
+	level_up_notification.visible = battle_manager.player_actor.status.skill_points_available > 0
 
 func _on_button_pressed() -> void:
 	Signals.toggle_menu.emit(Refs.Menu.REST)
 	battle_manager.game_run.start_next_battle()
+
+
+func _on_level_up_pressed() -> void:
+	Signals.toggle_menu.emit(Refs.Menu.CHARACTER)
