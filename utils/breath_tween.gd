a7768010f39ffc0beb3f5e18953cf51b6a0b25eb
Freddy Ebermann <freddy@42berlin.de>
2025-07-06 16:21:44 +0200

    account for starting scale

--- utils/breath_tween.gd
+++ utils/breath_tween.gd
@@ -7,11 +7,13 @@ var tween: Tween
 @onready var parent = get_parent()
 
 func _ready():
+	var starting_scale: Vector2 = parent.scale
+	var breath_scale: Vector2 = starting_scale * intensity
 	await get_tree().create_timer(randf_range(0, speed)).timeout
 	tween = create_tween()
 	tween.set_loops()  # Loop forever
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN_OUT)
 
-	tween.tween_property(parent, "scale", intensity, speed)
-	tween.tween_property(parent, "scale", Vector2(1.0, 1.0), speed)
+	tween.tween_property(parent, "scale", breath_scale, speed)
+	tween.tween_property(parent, "scale", starting_scale, speed)
