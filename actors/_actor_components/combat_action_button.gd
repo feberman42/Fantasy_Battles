aa556e508a32bd9864959828d02cfa4f3925ad32
Freddy Ebermann <freddy@42berlin.de>
2025-07-06 16:18:45 +0200

    redid combat action list init for performance and new tooltips

--- actors/_actor_components/combat_action_button.gd
+++ actors/_actor_components/combat_action_button.gd
@@ -6,12 +6,14 @@ var combat_action: BasicCombatAction
 var hover: bool = false
 var rect: Rect2
 var tooltip: String
+var tooltip_pos: Vector2
 
 func initialize(_combat_action: BasicCombatAction):
 	self.combat_action = _combat_action
 	self.text = str(_combat_action)
 	self.pressed.connect(_on_pressed)
 	tooltip = combat_action.generate_tooltip()
+	tooltip_pos = Vector2(global_position.x + (size.x * get_global_transform().get_scale().x), global_position.y)
 
 func _on_pressed() -> void:
 	action_button_pressed.emit(self.combat_action)
--- actors/_actor_components/combat_actions_list.gd
+++ actors/_actor_components/combat_actions_list.gd
@@ -5,18 +5,26 @@ signal action_selected
 @onready var button_container: VBoxContainer = $ButtonContainer
 var button_scene: PackedScene = preload("res://actors/_actor_components/combat_action_button.tscn") 
 
-func update_list(combat_actions: Array[BasicCombatAction], status: Status) -> void:
-	for button in button_container.get_children():
-		button.queue_free()
-	await get_tree().process_frame # wait for old buttons to be deleted
-	for action in combat_actions:
+func _ready() -> void:
+	for i in range(10):
 		var button: CombatActionButton = button_scene.instantiate()
+		button_container.add_child(button)
+		button.visible = false
+
+func update_list(combat_actions: Array[BasicCombatAction], status: Status) -> void:
+	self.visible = false
+	var buttons: Array[Node] = button_container.get_children()
+	for button in buttons:
+		button.visible = false
+	for i in range(combat_actions.size()):
+		var button: CombatActionButton = buttons[i] as CombatActionButton
+		var action: BasicCombatAction = combat_actions[i]
 		button.initialize(action)
 		button.action_button_pressed.connect(_on_button_pressed)
 		if action.energy_cost > status.current_energy:
 			button.disabled = true
 			button.focus_mode = Control.FOCUS_NONE
-		button_container.add_child(button)
+		button.visible = true
 
 func _on_button_pressed(action: BasicCombatAction):
 	action_selected.emit(action)
