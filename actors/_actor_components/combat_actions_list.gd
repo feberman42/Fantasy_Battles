eb2108b0c6b5e5470ab9fb0bd13fdbbb529df451
Freddy Ebermann <freddy@42berlin.de>
2025-07-07 15:37:29 +0200

    fixed action button siganl connect

--- actors/_actor_components/combat_actions_list.gd
+++ actors/_actor_components/combat_actions_list.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 	for i in range(10):
 		var button: CombatActionButton = button_scene.instantiate()
 		button_container.add_child(button)
+		button.action_button_pressed.connect(_on_button_pressed)
 		button.visible = false
 
 func update_list(combat_actions: Array[BasicCombatAction], status: Status) -> void:
@@ -20,7 +21,6 @@ func update_list(combat_actions: Array[BasicCombatAction], status: Status) -> vo
 		var button: CombatActionButton = buttons[i] as CombatActionButton
 		var action: BasicCombatAction = combat_actions[i]
 		button.initialize(action)
-		button.action_button_pressed.connect(_on_button_pressed)
 		if action.energy_cost > status.current_energy:
 			button.disabled = true
 			button.focus_mode = Control.FOCUS_NONE
